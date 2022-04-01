40fb80e79db8b649fa1cb41982ceea23f3987080
RISHIRAJ CHOWDHURY <rishiraj5565@gmail.com>
2022-04-02 01:49:36 +0530

    some updations

--- main.py
+++ main.py
@@ -14,14 +14,12 @@ def estimate():
     if request.method=='POST':
         result=request.form
         location = result['location']
-        openings = result['openings']
-        applicants = result['applicants']
         title = result['title']
         perks = result.getlist('perks')
         duration = result['duration']
         skills = result.getlist('skills')
 
-        pkl_file = open('index_dict.pkl', 'rb')
+        pkl_file = open('flask-app\index_dict.pkl', 'rb')
         index_dict = pickle.load(pkl_file)
         cat_vector = np.zeros(len(index_dict))
         
@@ -39,10 +37,6 @@ def estimate():
         except:
             pass
         try:
-            cat_vector[index_dict['Number of Openings']] = int(openings)
-        except:
-            pass
-        try:
             cat_vector[perk_list] = 1
         except:
             pass
@@ -55,22 +49,18 @@ def estimate():
         except:
             pass
         try:
-            cat_vector[index_dict['Applicants_'+str(applicants)]] = 1
-        except:
-            pass
-        try:
             cat_vector[skill_list] = 1
         except:
             pass
 
-        pkl_file = open('internshala_lgbm_model.pkl', 'rb')
+        pkl_file = open('flask-app\internshala_lgbm_model.pkl', 'rb')
         model = pickle.load(pkl_file)
 
-        scaler_file = open("scaler.pkl", "rb")
+        scaler_file = open("flask-app\scaler.pkl", "rb")
         scaler = pickle.load(scaler_file)
 
         prediction = model.predict(scaler.transform(cat_vector.reshape(1,-1)))
-        lmda = 0.2731531291764417
+        lmda = 0.49576864409121985
         prediction = inv_boxcox(prediction, lmda)
         pred = "Rs. " + str(int((prediction).tolist()[0])) + " approx."
         
